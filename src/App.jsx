c685b8bf4b4f6f4bd14717087fc1627c94f4bdfb
Rajeev <pmdbrajeev1@.com>
2024-11-11 13:24:43 +0530

    Added filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 
+const users = [
+  { firstName: "John", id: 1 },
+  { firstName: "Emily", id: 2 },
+  { firstName: "Michael", id: 3 },
+  { firstName: "Sarah", id: 4 },
+  { firstName: "David", id: 5 },
+  { firstName: "Jessica", id: 6 },
+  { firstName: "Daniel", id: 7 },
+  { firstName: "Olivia", id: 8 },
+  { firstName: "Matthew", id: 9 },
+  { firstName: "Sophia", id: 10 },
+];
+
 function App() {
   const [searchInput, setSearchInput] = useState("");
+  const [filteredItem, setFilteredItem] = useState(users);
 
   const handleSearchChange = (event) => {
-    setSearchInput(event.target.value);
-    console.log(searchInput);
+    const input = event.target.value.toLowerCase();
+    setSearchInput(input);
+
+    const filteredNames = users.filter((user) =>
+      user.firstName.toLowerCase().includes(input)
+    );
+    setFilteredItem(filteredNames);
   };
 
   return (
@@ -16,6 +35,13 @@ function App() {
         value={searchInput}
         onChange={handleSearchChange}
       />
+      <ul>
+        {filteredItem.length > 0 ? (
+          filteredItem.map((item) => <li key={item.id}>{item.firstName}</li>)
+        ) : (
+          <li>No results found</li>
+        )}
+      </ul>
     </div>
   );
 }
